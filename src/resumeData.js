1c367db1b8add485abaec32287b40b735a6d02fe
Talent971123 <cheeyongchang1123@outlook.sg>
2023-10-19 02:02:04 -0700

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,129 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Chee Yong Chang",
+  role: "Senior Blockchain && Frontend Developer",
+  linkedinId: "cheeyongchang-73910fc",
+  skypeid: "live:.cid.6857fa7100874173",
+  roleDescription: "Fast Learner, Good Communicator",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/cheeyongchang-67ba08121/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/Talent971123",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "Senior Front-End Developer with 5+ years of proven leadership and meaningful contributions to teams of varying size and scope. Collaborating with a variety of companies which made me intimately familiar with modern web technologies like React, Angular, and Vue, and have built a number of smart, responsive websites. Passionate about going the extra mile to ensure the customer is unquestionably satisfied with the product or service provided.",
+  address: "Singapore, 501 Bukit Timah Road",
+  education: [
+    {
+      UniversityName: "National university of Singapore",
+      specialization: "computer science",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2017",
+      Achievements: "Bachelor’s degree, computer science",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Frontend Developer",
+      specialization: "VERIPARK, London,England",
+      MonthOfLeaving: "2020.5",
+      YearOfLeaving: "2023.9",
+      Achievements: [
+        "Worked in a team of 4 and developed a new e-commerce site based on a styled system, TypeScript and React",
+        "Created an admin panel and associated design system in React and Storybook to be used by clients and the internal admin",
+        "Rewrote HTML to meet industry and company standards for SEO and Accessibility",
+        "Tested, debugged, and shipped 10s of 1000s of lines of code to various development teams.",
+        "Worked full-stack on the team to develop a consumer-facing review platform with Node.js, React, GraphQL, Elasticsearch, and PostgreSQL.",
+        "Built optimization algorithms to automate advertising campaign management using Facebook's Marketing API, Python, Django, and PostgreSQL.",
+      ],
+    },
+    {
+      CompanyName: "Frontend Developer",
+      specialization: "Codevate, 9-11 Vittoria St",
+      MonthOfLeaving: "2018.3",
+      YearOfLeaving: "2020.5",
+      Achievements: [
+        "Implemented RESTful APIs in Django that increased reporting speed from the analytical team by 22%.",
+        "Utilized HTML, CSS, and JavaScript to create 250+ responsive landing pages for both company and client.",
+        "Worked closely with the back-end team to display data using the Custom Components, library Components, Redux, and Redux Saga.",
+        "Worked with Quality Assurance to get new pages/products tested,and addressed any issues within 48 hours.",
+      ],
+    },
+  ],
+  skillsDescription: "My skills here",
+  skills: [
+    {
+      skillname: "HTML",
+      rate: "100%",
+    },
+    {
+      skillname: "CSS, SCSS, SASS",
+      rate: "100%",
+    },
+    {
+      skillname: "Javascript",
+      rate: "100%",
+    },
+    {
+      skillname: "React, Jquery",
+      rate: "100%",
+    },
+    {
+      skillname: "Angular, Vue",
+      rate: "100%",
+    },
+    {
+      skillname: "Django",
+      rate: "100%",
+    },
+    {
+      skillname: "Blockchain, Smart contract",
+      rate: "90%",
+    },
+    {
+      skillname: "Blockchain, Smart contract",
+      rate: "100%",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
